352ad850776884ac2bdc14f39c08b890a0cdaf51
ralphS16 <sarkr16@gmail.com>
2025-07-17 16:21:26 +0100

    Removed all the js except the random links.

--- script.js
+++ script.js
@@ -97,28 +97,6 @@ const videos = [
 	'8kAcmqoQE_U'
 ];
 
-function openProject(e){
-	card = document.getElementById(e.target.id+'-card');
-	if (card.style.display == 'table-row'){
-		card.style.display = 'none';
-	} else {
-		for (let proj of document.getElementsByClassName('project-card')){
-			proj.style.display = 'none';
-		}
-		card.style.display = 'table-row';
-	}
-	window.location.hash = '#'+e.target.id;
-}
-window.onload = function(){
-	for (let proj of document.getElementsByClassName('project-title')){
-		proj.addEventListener('click', openProject,false);
-	}
-	let hash = window.location.hash.substring(1);
-	if (hash) {
-		document.getElementById(window.location.hash.substring(1)+"-card").style.display = 'table-row';
-		window.location.hash = '#'+hash;
-	}	
-}
 
 function randomVideo(){
 	var i = parseInt(Math.random() * videos.length);
